ae2c753288f688cae82ab72f5a4235983f658ac4
bbardi <bbardi.dev@gmail.com>
2023-10-24 13:02:09 +0800

    scroll fix, connection count

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
-import { NEW_MESSAGE_CHANNEL } from "@/lib/constants";
+import {
+  CONNECTION_COUNT_UPDATED_CHANNEL,
+  NEW_MESSAGE_CHANNEL,
+} from "@/lib/constants";
 import useSocket from "@/lib/hooks/useSocket";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 type Message = {
   id: string;
@@ -11,9 +14,18 @@ type Message = {
 };
 
 export default function Home() {
+  const messageListRef = useRef<HTMLOListElement | null>(null);
   const socket = useSocket();
   const [newMessage, setNewMessage] = useState<string>("");
   const [messages, setMessages] = useState<Message[]>([]);
+  const [connectionCount, setConnectionCount] = useState(0);
+
+  function scrollToBottom() {
+    if (messageListRef.current) {
+      messageListRef.current.scrollTop =
+        messageListRef.current.scrollHeight + 1000;
+    }
+  }
 
   useEffect(() => {
     socket?.on("connect", () => {
@@ -22,7 +34,17 @@ export default function Home() {
 
     socket?.on(NEW_MESSAGE_CHANNEL, (message: Message) => {
       setMessages((prevMessages) => [...prevMessages, message]);
+      setTimeout(() => {
+        scrollToBottom();
+      }, 0);
     });
+
+    socket?.on(
+      CONNECTION_COUNT_UPDATED_CHANNEL,
+      ({ count }: { count: number }) => {
+        setConnectionCount(count);
+      }
+    );
   }, [socket]);
 
   function handleSubmit(e: React.FormEvent<HTMLFormElement> | null) {
@@ -33,10 +55,13 @@ export default function Home() {
   }
 
   return (
-    <main className="flex flex-col gap-20 bg-slate-900 items-center p-32 h-screen text-3xl justify-center mx-auto min-h-full w-full">
-      <div className="flex flex-col gap-10 p-10 w-full bg-slate-300 rounded-md max-h-[720px] overflow-y-scroll">
+    <main className="flex flex-col gap-12 bg-slate-900 items-center p-24 h-screen text-3xl justify-center mx-auto min-h-full w-full">
+      <ol
+        ref={messageListRef}
+        className="flex flex-col gap-10 p-10 w-full bg-slate-300 rounded-md max-h-[840px] overflow-y-scroll"
+      >
         {messages.map((message, i) => (
-          <div
+          <li
             key={message.id}
             className={` bg-indigo-900 p-6 rounded-xl w-max text-slate-50 flex gap-2 ${
               i % 2 === 0
@@ -46,9 +71,10 @@ export default function Home() {
           >
             <span>&quot;{message.message}&quot;</span>
             <span>at: {message.createdAt}</span>
-          </div>
+          </li>
         ))}
-      </div>
+      </ol>
+      <h2>Connection count: {connectionCount}</h2>
       <form onSubmit={handleSubmit} className="flex w-full flex-col gap-6">
         <Textarea
           className="rounded-lg bg-slate-100 p-2 text-xl"
